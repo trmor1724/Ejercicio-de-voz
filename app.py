68842c062b57beb01cf6017e250f9ee6c2623a48
tremor1724 <148923646+trmor1724@users.noreply.github.com>
2025-05-01 21:00:38 -0500

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from gtts import gTTS
 from PIL import Image
 
 st.title("Interfases Multimodales.")
-image = Image.open('text_to_audio.png')
+image = Image.open('9568738.png')
 
 st.image(image, width=200)
 
